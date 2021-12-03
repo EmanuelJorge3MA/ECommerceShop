7150b14bde1efa4ddea321ec8d87caebfae9952d
emanueljorge <emanuel.jorge.130@gmail.com>
2021-12-03 15:05:31 +0100

    LaunchScreen

---
+++ Shared/View/OnBoardingPage.swift
@@ -0,0 +1,65 @@
+//
+//  OnBoardingPage.swift
+//  ECommerceShop (iOS)
+//
+//  Created by Emanuel Jorge on 03/12/21.
+//
+
+import SwiftUI
+
+// To Use the custom font on all pages...
+let customFont = "Raleway-Regular"
+
+struct OnBoardingPage: View {
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Text("Find your\nGadget")
+                .font(.custom(customFont, size: 55))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            
+            Image("gadget")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Button {
+                
+            } label: {
+                Text("Get started")
+                    .font(.custom(customFont, size: 18))
+                    .fontWeight(.semibold)
+                    .padding(.vertical, 18)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 5, y: 5)
+                    .foregroundColor(.purple) // MARK: - Definir Cor no Assets
+            }
+            .padding(.horizontal, 30)
+            // Adding Some Adjustments Only for larger displays...
+            .offset(y: getRect().height < 750 ? 20 : 40)
+            
+            Spacer()
+        }
+        .padding()
+        .padding(.top, getRect().height < 750 ? 0 : 20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            Color(.purple) // MARK: - Definir Cor no Assets
+        )
+    }
+}
+
+struct OnBoardingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingPage()
+    }
+}
+
+// Extending View to get Screen Bounds...
+extension View {
+    func getRect()->CGRect {
+        return UIScreen.main.bounds
+    }
+}
