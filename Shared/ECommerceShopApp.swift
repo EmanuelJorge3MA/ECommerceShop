ccb985f59c3ee6ae40029f9164741aefe224a01c
emanueljorge <emanuel.jorge.130@gmail.com>
2021-12-03 12:52:34 +0100

    Initial Commit

---
+++ Shared/ECommerceShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceShopApp.swift
+//  Shared
+//
+//  Created by Emanuel Jorge on 03/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
